9dcc59f6aeee3c0d5f4fd10a159b904ae93d408a
Oluwaseyi Ogunjinmi <80404459+Oluwaseyieniola@users.noreply.github.com>
2023-09-24 20:23:05 +0100

    first commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<flutter_share_me/SwiftFlutterShareMePlugin.h>)
+#import <flutter_share_me/SwiftFlutterShareMePlugin.h>
+#else
+@import flutter_share_me;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [SwiftFlutterShareMePlugin registerWithRegistrar:[registry registrarForPlugin:@"SwiftFlutterShareMePlugin"]];
+}
+
+@end
